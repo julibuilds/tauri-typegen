de5b88bc2bac1a63e9d8966b107372de91662b44
Stefan Poindl <stefan@tohuwabohu.io>
2025-12-11 23:54:35 +0100

    remove type_resolver where not needed

--- src/analysis/mod.rs
+++ src/analysis/mod.rs
@@ -101,7 +101,6 @@ impl CommandAnalyzer {
                             func,
                             &command.name,
                             parsed_file.path.as_path(),
-                            &mut self.type_resolver,
                         )?;
 
                         // Collect type names from channel message types
@@ -114,11 +113,9 @@ impl CommandAnalyzer {
                 }
 
                 // Extract events from this file's AST
-                let file_events = self.event_parser.extract_events_from_ast(
-                    &parsed_file.ast,
-                    parsed_file.path.as_path(),
-                    &mut self.type_resolver,
-                )?;
+                let file_events = self
+                    .event_parser
+                    .extract_events_from_ast(&parsed_file.ast, parsed_file.path.as_path())?;
 
                 // Collect type names from command parameters and return types using functional style
                 file_commands.iter().for_each(|cmd| {
@@ -190,11 +187,9 @@ impl CommandAnalyzer {
                 // Extract commands and events from the cached AST
                 if let Some(parsed_file) = self.ast_cache.get_cloned(&path_buf) {
                     // Extract events
-                    let file_events = self.event_parser.extract_events_from_ast(
-                        &parsed_file.ast,
-                        path_buf.as_path(),
-                        &mut self.type_resolver,
-                    )?;
+                    let file_events = self
+                        .event_parser
+                        .extract_events_from_ast(&parsed_file.ast, path_buf.as_path())?;
                     self.discovered_events.extend(file_events);
 
                     // Extract commands
@@ -213,8 +208,8 @@ impl CommandAnalyzer {
                                 func,
                                 &command.name,
                                 path_buf.as_path(),
-                                &mut self.type_resolver,
                             )?;
+
                             command.channels = channels;
                         }
                     }
@@ -339,11 +334,7 @@ impl CommandAnalyzer {
                     if item_enum.ident == type_name
                         && self.struct_parser.should_include_enum(item_enum)
                     {
-                        return self.struct_parser.parse_enum(
-                            item_enum,
-                            file_path,
-                            &mut self.type_resolver,
-                        );
+                        return self.struct_parser.parse_enum(item_enum, file_path);
                     }
                 }
                 _ => {}
