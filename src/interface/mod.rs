45a66fd9d1ae07074608e89d113482cc061206a2
Stefan Poindl <stefan@tohuwabohu.io>
2025-12-11 23:40:54 +0100

    apply visitor pattern

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -59,7 +59,7 @@ pub fn generate_from_config(
                 let optional = if field.is_optional { "?" } else { "" };
                 logger.verbose(&format!(
                     "    • {}{}: {} ({})",
-                    field.name, optional, field.typescript_type, visibility
+                    field.name, optional, field.rust_type, visibility
                 ));
             }
         }
